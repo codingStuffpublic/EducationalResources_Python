a43afc4f8418154f30b8cf4b9db81f6de78db4ab
Sophie <zsofiliszkai@gmail.com>
2025-01-18 17:07:58 +0100

    Password Generator app using random, join, list_comprehension.

---
+++ pwd_generator.py
@@ -0,0 +1,16 @@
+import random
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+               's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+               'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [random.choice(letters) for l in range(random.randint(8, 10))]
+    password_list += [random.choice(symbols) for l in range(random.randint(2, 4))]
+    password_list += [random.choice(numbers) for l in range(random.randint(2, 4))]
+    random.shuffle(password_list)
+
+    to_string = "".join(password_list)
+    return to_string
